0db18833c4a1cfc716c45a76c2e6653e6365aa67
cesarcor <cesarcorrechel@gmail.com>
2021-10-20 23:53:36 -0600

    add/fix url prop

--- src/components/outline-button/outline-button.jsx
+++ src/components/outline-button/outline-button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './outline-button.module.scss';
 
 export default function OutlineButton(props) {
-    return (
-        <a href="#" className={styles.outline_btn}>
-            {props.btn_text}
-        </a>
-    )
+  return (
+    <a href={props.btn_url} className={styles.outline_btn} target='_blank'>
+      {props.btn_text}
+    </a>
+  );
 }
